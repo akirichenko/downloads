16ac4bc783f136daf2bc23ea83350baeebc6f8ab
akirichenko <avkirichenko@gmail.com>
2024-05-15 00:13:46 +0100

    Create renice_script.sh

---
+++ renice_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# add to /etc/sysctl.conf
+# kernel.sched_autogroup_enabled = 0
+# check cat /proc/sys/kernel/sched_autogroup_enabled
+# and reset manually echo 0 > /proc/sys/kernel/sched_autogroup_enabled
+# use /etc/sysctl.conf from above
+
+arr=("polkitd" "microsoft" "home1" "auditd")
+
+for i in "${!arr[@]}"; do
+  for p in $(pgrep -f ${arr[$i]}); do
+    # process itself
+    sudo renice +19 -p $p
+    # child processes
+    for c in $(pgrep -P $p); do
+      sudo renice +19 -p $p
+    done
+  done
+  # all threads
+  for p in $(pgrep -f -w ${arr[$i]}); do
+    sudo renice +19 -p $p
+  done
+done
