5192de135d9542f45bac7ea4e8c89f8bcd7f5118
akirichenko <avkirichenko@gmail.com>
2024-05-15 00:28:10 +0100

    Update renice_script.sh

--- renice_script.sh
+++ renice_script.sh
@@ -6,7 +6,7 @@
 # check: cat /proc/sys/kernel/sched_autogroup_enabled
 # and reset manually: echo 0 > /proc/sys/kernel/sched_autogroup_enabled
 # use /etc/sysctl.conf from above
-# or add "noautogroup" to the kernel cmdline
+# or add "noautogroup" to the kernel cmdline !!! better this one
 
 arr=("polkitd" "microsoft" "home1" "auditd")
 
